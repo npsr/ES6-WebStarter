81a2902443730c5df0421af6492b51d9f834f9f9
lzsy <lukasz.samorodny@gft.com>
2017-10-03 16:03:52 +0200

    scss support

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
-const pack = require('../package.json')
-const homeTpl = require('components/home/home.pug')
+const pack = require('../package.json');
+const homeTpl = require('components/home/home.pug');
 
-console.info('Welcome to ' + pack.name + ', version:' + pack.version)
-document.getElementById('root').innerHTML = homeTpl
+console.info('Welcome to ' + pack.name + ', version:' + pack.version);
+document.getElementById('root').innerHTML = homeTpl;
