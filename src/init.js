a88ce62a8d6b9f140590d49e4b74231d20f83235
lzsy <lukasz.samorodny@gft.com>
2017-10-19 10:30:50 +0200

    name change

--- src/init.js
+++ src/init.js
@@ -2,4 +2,4 @@ const pack = require('../package.json');
 const homeTpl = require('components/home/home.pug');
 
 console.info('Welcome to ' + pack.name + ', version:' + pack.version);
-document.getElementById('root').innerHTML = homeTpl;
+document.querySelector('#root').innerHTML = homeTpl;
