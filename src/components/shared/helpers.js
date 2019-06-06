b941c5732b85b302173d2b0bc3d552a5f63aafa6
npsr <napster1321@gmail.com>
2019-06-06 14:51:38 +0200

    Helpers update

--- src/components/shared/helpers.js
+++ src/components/shared/helpers.js
@@ -4,26 +4,32 @@ import { fromEvent } from 'rxjs';
 class Helpers {
   // ---------------------------------------------------------------------------
   static addCSS(css: string) {
+
     const style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = css;
 
     fromEvent(document, 'DOMContentLoaded').subscribe(() => {
-      document.querySelector('head').appendChild(style);
+      const head = document.querySelector('head');
+      if (head) head.appendChild(style);
     });
   }
 
   // ---------------------------------------------------------------------------
-  static addHTML(target: HTMLElement, content: string) {
+  static addHTML(target: string, content: string) {
+
     fromEvent(document, 'DOMContentLoaded').subscribe(() => {
-      document.querySelector(target || '#root').innerHTML = content;
+      const root = document.querySelector(target || '#root');
+      if (root) root.innerHTML = content;
     });
   }
 
   // ---------------------------------------------------------------------------
-  static addIMG(target: HTMLElement, src: string) {
+  static addIMG(target: string, src: string) {
+
     fromEvent(document, 'DOMContentLoaded').subscribe(() => {
-      document.querySelector(target).src = src;
+      const img = document.querySelector(`img ${target}`);
+      if (img) img.setAttribute('src', src);
     });
   }
 }
