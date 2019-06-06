4a812774d96e0d73782338d1c22077c913421eb5
npsr <napster1321@gmail.com>
2019-06-06 14:56:41 +0200

    contact info

--- src/components/shared/helpers.js
+++ src/components/shared/helpers.js
@@ -4,7 +4,6 @@ import { fromEvent } from 'rxjs';
 class Helpers {
   // ---------------------------------------------------------------------------
   static addCSS(css: string) {
-
     const style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = css;
@@ -17,7 +16,6 @@ class Helpers {
 
   // ---------------------------------------------------------------------------
   static addHTML(target: string, content: string) {
-
     fromEvent(document, 'DOMContentLoaded').subscribe(() => {
       const root = document.querySelector(target || '#root');
       if (root) root.innerHTML = content;
@@ -26,7 +24,6 @@ class Helpers {
 
   // ---------------------------------------------------------------------------
   static addIMG(target: string, src: string) {
-
     fromEvent(document, 'DOMContentLoaded').subscribe(() => {
       const img = document.querySelector(`img ${target}`);
       if (img) img.setAttribute('src', src);
